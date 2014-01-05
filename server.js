1517d7f8e3ee0ccb8a775520a343bf3953606e61
Zack Bloom <zackbloom@gmail.com>
2014-01-04 19:44:47 -0500

    Switch to request module to get around response issue

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var url     = require('url'),
     https   = require('https'),
     fs      = require('fs'),
     qs      = require('querystring'),
+    request = require('request'),
     express = require('express'),
     app     = express();
 
@@ -21,35 +22,24 @@ function loadConfig() {
 var config = loadConfig();
 
 function authenticate(code, cb) {
-  var data = qs.stringify({
+  var data = {
     client_id: config.oauth_client_id,
     client_secret: config.oauth_client_secret,
     code: code
-  });
-
-  var reqOptions = {
-    host: config.oauth_host,
-    port: config.oauth_port,
-    path: config.oauth_path,
-    method: config.oauth_method,
-    headers: {
-      'content-length': data.length,
-      'accept': 'application/json'
-    }
   };
 
-  var body = "";
-  var req = https.request(reqOptions, function(res) {
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) { body += chunk; });
-    res.on('end', function() {
-      cb(null, qs.parse(body).access_token);
-    });
-  });
+  var reqOptions = {
+    url: config.oauth_host + ':' + config.oauth_port + config.oauth_path,
+    method: config.oauth_method,
+    form: data
+  };
 
-  req.write(data);
-  req.end();
-  req.on('error', function(e) { cb(e.message); });
+  request(reqOptions, function(err, resp, body) {
+    if (err)
+      cb(err);
+    else
+      cb(null, qs.parse(body).access_token);
+  });
 }
 
 
